0128de24129a420b16974efc23ac3e4474ad5321
RichRavenM <richard.s.morris@btinternet.com>
2023-10-15 12:36:21 +0100

    adds logic1 project

---
+++ LogicProject1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
+
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+
+// int saleAmount = 1001;
+// // int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+
+// Random x = new Random();
+
+// Console.WriteLine($"{(x.Next(2) == 1 ? "heads" : "tails" )}");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin") && level > 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin"))
+{
+    Console.WriteLine("Welcome, Admin user.");  
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (permission.Contains("Manager"))
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+else 
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
