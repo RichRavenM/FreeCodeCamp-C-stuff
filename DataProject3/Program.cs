f9cda16e0d4df745995ac20e992741f178e90bc0
RichRavenM <richard.s.morris@btinternet.com>
2023-10-16 13:59:36 +0100

    adds basic array methods data project

---
+++ DataProject3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+/*
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+*/
+Console.Clear();
+/*
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+*/
+/*
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(",");
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+*/
+
+/*
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] items = pangram.Split(" ");
+
+for(int i = 0; i < items.Length; i++)
+{
+    char[] wordArray = items[i].ToCharArray();
+    Array.Reverse(wordArray);
+    items[i] = new string(wordArray);
+}
+string result = String.Join(" ", items);
+
+Console.WriteLine(result);
+*/
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orders = orderStream.Split(",");
+Array.Sort(orders);
+
+foreach(string i in orders)
+{
+    if(i.Length == 4)
+    {
+        Console.WriteLine(i);
+    }
+    else
+    {
+        Console.WriteLine($"{i}\t - Error");
+    }
+}
